1e6c0bf6e26f12d29eece5e48fa2714d0dfbd89c
SuperSonic <supersonic@livemail.tw>
2021-08-03 13:06:19 +0800

    Merge `tools` into `service`

--- service/talk/UpdateSeen.go
+++ service/talk/UpdateSeen.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package seen
+package talk
 
 import "gopkg.in/star-inc/kaguyakernel.v2/data"
 
