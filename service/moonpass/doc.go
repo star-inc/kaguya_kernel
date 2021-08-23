fdef7316a2f0b774b0c7651c4cd1a003325cd7d0
SuperSonic <supersonic@livemail.tw>
2021-08-23 21:45:45 +0800

    Upload doc.go of moonpass

---
+++ service/moonpass/doc.go
@@ -0,0 +1,4 @@
+// Package moonpass an e2ee technology based on SIDH/SIKE and Double Ratchet,
+// to be used for passing keys between clients and their contacts.
+// The idea is from https://blog.cloudflare.com/sidh-go, thanks to CloudFlare.
+package moonpass
